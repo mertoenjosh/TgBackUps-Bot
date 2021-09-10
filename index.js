5e7ec08844beaa45ece01a98aac7fb29afbfbce1
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-10 15:16:20 +0300

    commit

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const { Telegraf } = require('telegraf');
+const axios = require('axios');
+const fs = require('fs');
 const dotenv = require('dotenv');
 
 // configure environment
 dotenv.config({ path: '.env' });
+
+// toekn initializtion
+const token = process.env.BOT_TOKEN;
+
+if (token === undefined) {
+  throw new Error('Token must be provided');
+}
+
+// Bot instatiation
+const bot = new Telegraf(token);
 
 // replys initializations
 
 const welcomeMsg = `Welcome to Telegram Documents Backup\nType '/help' for help`;
 const helpMsp = ``;
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// Start
 bot.start(ctx => ctx.reply(welcomeMsg));
-bot.help(ctx => ctx.reply('Send me a sticker'));
-bot.hears('hi', ctx => ctx.reply('Hey there'));
 
-// Handle sticker or photo update
-bot.on(['sticker', 'photo'], ctx => {
+// help init
+bot.help(ctx => ctx.reply('Send me a sticker'));
+
+// handle media
+const media = ['photo', 'document'];
+
+bot.on('photo', async ctx => {
   console.log(ctx.message);
-  return ctx.reply('Cool!');
+  const { file_id } = ctx.message.photo.pop();
+
+  const url = await ctx.telegram.getFileLink(file_id);
+  ctx.reply(url);
+});
+
+// hande cocument
+
+bot.on('document', async ctx => {
+  const { file_id: fileId } = ctx.update.message.document;
+  const fileUrl = await ctx.telegram.getFileLink(fileId);
+  const response = await axios.get(fileUrl);
+  ctx.reply('I read the file for you! The contents were:\n\n' + response.data);
 });
 
 bot.launch();
