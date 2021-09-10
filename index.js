7735cc08e9bd974fe8f8a8439145a8a220266c9b
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-10 13:54:26 +0300

    commit

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const { Telegraf } = require("telegraf");
-const dotenv = require("dotenv");
+const { Telegraf } = require('telegraf');
+const dotenv = require('dotenv');
 
 // configure environment
-dotenv.config({ path: ".env" });
+dotenv.config({ path: '.env' });
+
+// replys initializations
+
+const welcomeMsg = `Welcome to Telegram Documents Backup\nType '/help' for help`;
+const helpMsp = ``;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.launch();
+bot.start(ctx => ctx.reply(welcomeMsg));
+bot.help(ctx => ctx.reply('Send me a sticker'));
+bot.hears('hi', ctx => ctx.reply('Hey there'));
 
 // Handle sticker or photo update
-bot.on(["sticker", "photo"], (ctx) => {
+bot.on(['sticker', 'photo'], ctx => {
   console.log(ctx.message);
-  return ctx.reply("Cool!");
+  return ctx.reply('Cool!');
 });
+
+bot.launch();
